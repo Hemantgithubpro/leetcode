b95a67cbdd62719fbc81af0ee4700c9a2393c253
Hemant <hemantjha2005@gmail.com>
2025-03-07 19:40:43 +0530

    did some changes for the 89th problem and made som e notes for it

--- 89/myowncode.cpp
+++ 89/myowncode.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int decimaltobinary(int n){
+    int binary = 0;
+    int rem, i = 1;
+    while(n!=0){
+        rem = n%2;
+        n = n/2;
+        binary = binary + (rem*i);
+        i = i*10;
+    }
+    return binary;
+}
+
+int binaryToGray(int num){
+    return num ^ (num >> 1);
+}
+
+vector<int> grayCode(int n) {
+    vector<int> ans;
+    // for(int i=0; )
+        
+}
+
 int main(){
+    // cout<<binaryToGray(7)<<endl;
+    // cout<<decimaltobinary(7)<<endl;
+    // string str1="231";
+    // int myint1=stoi(str1);
+    // cout<<myint1;
     
+    return 0;
 }
