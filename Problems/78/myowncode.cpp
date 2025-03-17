d0bd2b1d843d830cb0d7da5d3db3762518955a20
Hemant <hemantjha2005@gmail.com>
2025-03-17 21:08:55 +0530

    some correction

--- Problems/78/myowncode.cpp
+++ Problems/78/myowncode.cpp
@@ -6,7 +6,7 @@ vector<vector<int>> subsets(vector<int>& nums) {
     vector<vector<int>> ans;
     for(int num=0;num<subsets;num++){
         vector<int> l;
-        for(int i=0; i<num; i++){
+        for(int i=0; i<nums.size(); i++){
             if(num&(1<<i)) l.push_back(nums[i]);
         }
         ans.push_back(l);
