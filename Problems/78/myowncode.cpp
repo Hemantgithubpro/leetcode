323957d2e0870602f5e04114727729d4ed9256be
Hemant <hemantjha2005@gmail.com>
2025-03-17 21:08:16 +0530

    did 78

---
+++ Problems/78/myowncode.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    int subsets = 1 << nums.size();
+    vector<vector<int>> ans;
+    for(int num=0;num<subsets;num++){
+        vector<int> l;
+        for(int i=0; i<num; i++){
+            if(num&(1<<i)) l.push_back(nums[i]);
+        }
+        ans.push_back(l);
+    }
+    return ans;
+}
+
+int main(){
+    
+}
