16b32d25827150fb1741c0e08be509c592ff74d1
Hemant <hemantjha2005@gmail.com>
2025-03-18 18:11:37 +0530

    did single number II

---
+++ Problems/137/myowncode.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    int ones=0, twos=0;
+    for(int i=0; i<nums.size(); i++){
+        ones=(ones^nums[i]) & ~twos;
+        twos=(twos^nums[i]) & ~ones;
+    }
+    return ones;
+}
+
+int main(){
+    vector<int> n={2,2,3,2};
+    cout<<singleNumber(n);
+    
+}
