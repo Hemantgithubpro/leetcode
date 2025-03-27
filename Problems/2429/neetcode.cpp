b847dc464fd6dac0ac8df9a31f617b952fc9b0d7
Hemant <hemantjha2005@gmail.com>
2025-03-27 22:04:25 +0530

    wrote full code of 2429

--- Problems/2429/neetcode.cpp
+++ Problems/2429/neetcode.cpp
@@ -1,8 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int count_bits(int n){
+    int res=0;
+    while(n>0){
+        res+=1&n;
+        n=n>>1;
+    } 
+    return res;
+}
+
 int minimizeXor(int num1, int num2) {
-        
+    int count1=count_bits(num1);
+    int count2=count_bits(num2);
+    int x=num1;
+
+    int i=0;
+    // remove least significant bit
+        while(count1>count2){
+            if(x&(1<<i)) {
+                count1--;
+                x=x^(1<<i);
+            }
+            i++;
+        }
+
+    // adding leas significant bit
+    while(count1<count2){
+        if((x&(1<<i))==0) {
+            count1++;
+            x=x|(1<<i);
+        }
+        i++;
+    }
+    return x;
 }
 
 int main(){
