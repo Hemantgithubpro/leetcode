511701c09a794d618f5dc8a657ad5e667b13913b
Hemant <hemantjha2005@gmail.com>
2025-03-29 22:23:12 +0530

    used unordered map for solution

--- Problems/3/myowncode.cpp
+++ Problems/3/myowncode.cpp
@@ -4,34 +4,41 @@ using namespace std;
 int lengthOfLongestSubstring(string s) {
     // since its string, look ascii table a->97
     // ex. abcabcbb
-    
-    // without unordered_map
     int l=0;
     int r=0;
     int len=0;
     int maxlen=0;
-    // int hash[256]={-1};
-    vector<int> hash(256,-1);
-    while(r<s.size()){
-        if(hash[s[r]]!=-1){ // its in hashset
-            if(hash[s[r]]>=l){
-                l=hash[s[r]]+1;
-            }
+    
+    // without unordered_map
+    // // int hash[256]={-1};
+    // vector<int> hash(256,-1);
+    // while(r<s.size()){
+    //     if(hash[s[r]]!=-1){ // its in hashset
+    //         if(hash[s[r]]>=l){
+    //             l=hash[s[r]]+1;
+    //         }
+    //     }
+    //     hash[s[r]]=r;
+    //     len=r-l+1;
+    //     r++;
+    //     maxlen=max(len,maxlen);
+    // }
+    // return maxlen;
+
+    // with unordered_map
+    unordered_map<char,int> mpp;
+    
+    for(int r=0; r<s.size(); r++){
+        auto it=mpp.find(s[r]);
+        if(it!=mpp.end()){  // if found
+            if(it->second>=l) l=mpp[s[r]]+1;
         }
-        hash[s[r]]=r;
+        mpp[s[r]]=r;
         len=r-l+1;
-        r++;
         maxlen=max(len,maxlen);
     }
     return maxlen;
 }
-    // unordered_map<char,int> mpp;
-    // for(int i=0; i<s.size(); i++){
-    //     auto it=mpp.find(s[i]);
-    //     if(it==mpp.end()){
-    //         mpp[s[i]]=i;
-    //     }
-    // }
 
 int main(){
     string s="abcabcbb";
