2bcbdb6684df89e79f5c208b02d14de997ad632b
Hemant <hemantjha2005@gmail.com>
2025-03-29 21:51:22 +0530

    completed the code

--- Problems/3/myowncode.cpp
+++ Problems/3/myowncode.cpp
@@ -4,17 +4,37 @@ using namespace std;
 int lengthOfLongestSubstring(string s) {
     // since its string, look ascii table a->97
     // ex. abcabcbb
-
+    
+    // without unordered_map
     int l=0;
     int r=0;
-    // unordered_map mpp;
-    for(int i=0; i<s.size(); i++){
-
+    int len=0;
+    int maxlen=0;
+    // int hash[256]={-1};
+    vector<int> hash(256,-1);
+    while(r<s.size()){
+        if(hash[s[r]]!=-1){ // its in hashset
+            if(hash[s[r]]>=l){
+                l=hash[s[r]]+1;
+            }
+        }
+        hash[s[r]]=r;
+        len=r-l+1;
+        r++;
+        maxlen=max(len,maxlen);
     }
+    return maxlen;
 }
+    // unordered_map<char,int> mpp;
+    // for(int i=0; i<s.size(); i++){
+    //     auto it=mpp.find(s[i]);
+    //     if(it==mpp.end()){
+    //         mpp[s[i]]=i;
+    //     }
+    // }
 
 int main(){
     string s="abcabcbb";
-    lengthOfLongestSubstring(s);
+    cout<<lengthOfLongestSubstring(s);
     return 0;
 }
