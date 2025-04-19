a416ad34ef759f93667cadb71e52cfd16e63c984
Hemant Jha <hemantjha2005@gmail.com>
2025-04-19 22:44:11 +0530

    todo again

---
+++ Problems/912/code.cpp
@@ -0,0 +1,12 @@
+// i'll do it again but i solved it
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortArray(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+    return nums;
+}
+
+int main(){
+    vector<int> nums={23,35,64,32,62,14,53,41,73};
+}
