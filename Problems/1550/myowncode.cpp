56cb9edbd5f9becbc93bc8d70ff1590c90e86428
Hemant Jha <hemantjha2005@gmail.com>
2025-05-11 15:20:24 +0530

    lt potd

---
+++ Problems/1550/myowncode.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+// i wonder why was this question even there.
+
+bool threeConsecutiveOdds(vector<int>& arr) {
+        if(arr.size()<3) return false;
+        for(int i=2;i<arr.size(); i++){
+            if(arr[i]%2==1 && arr[i-1]%2==1 && arr[i-2]%2==1) return true;
+        }
+        return false;
+    }
+
+int main(){
+    
+}
