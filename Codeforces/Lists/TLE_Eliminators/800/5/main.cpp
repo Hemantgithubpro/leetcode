68a9c241df8373cb2673ac2e5ea9851354f2a94b
Hemant Jha <hemantjha2005@gmail.com>
2025-06-10 13:32:53 +0530

    did 5th

--- Codeforces/Lists/TLE_Eliminators/800/5/main.cpp
+++ Codeforces/Lists/TLE_Eliminators/800/5/main.cpp
@@ -1,11 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    // code
+bool sorted(int n, vector<int> &a)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i - 1] > a[i])
+            return false;
+    }
+    return true;
 }
 
-int main() {
+void solve(int n, vector<int> &a)
+{
+    // code
+    for (int j = 0; j < n; j++)
+    {   
+        if(sorted(n,a)){
+            cout<<"YES"<<endl;
+            return;
+        }
+        for (int i = 1; i < n-1; i++)
+        {
+            if (a[i - 1] < a[i] && a[i] > a[i + 1])
+            swap(a[i], a[i + 1]);
+        }
+        if(sorted(n,a)){
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+}
+
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -17,9 +46,18 @@ int main() {
     // Your code goes here
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         // input
-        solve();
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        solve(n, a);
     }
 
     return 0;
---
+++ Codeforces/Lists/TLE_Eliminators/800/5/notes.md
@@ -0,0 +1,2 @@
+did it on my second try, the first time, it was accessing out of bounds in the array, so I had to fix that.
+and also my first intuition was correct, I just had to implement it correctly.
