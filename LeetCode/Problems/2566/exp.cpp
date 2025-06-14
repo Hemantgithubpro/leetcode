69fe18e128d1b48c4db691b375a28d525d8f1d5c
Hemant Jha <hemantjha2005@gmail.com>
2025-06-14 13:14:35 +0530

    lc potd

---
+++ LeetCode/Problems/2566/exp.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int a=34532;
+    string s=to_string(a);
+    cout<<s<<endl;    
+    cout<<s[0]<<s[2]<<endl;
+    int b=stoi(s);
+    // s
+}
---
+++ LeetCode/Problems/2566/myowncode.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+// did it
+int minMaxDifference(int num)
+{
+    string s = to_string(num);
+    unordered_map<char, int> um;
+    for (int i = 0; i < s.size(); i++)
+    {
+        um[s[i]]++;
+    }
+
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+
+    // for minimum number: find most frequent no. or the front most occurence int and make it '0' and positions don't change
+    string s1 = s;
+    int i = 0;
+    char first = s1[0];
+    while (s1[i] == '0' && i < s1.size() - 1)
+    {
+        i++;
+        first = s1[i];
+    }
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] == first)
+            s1[i] = '0';
+    }
+    mini = min(mini, stoi(s1));
+
+    // for maximum number: find most frequent no. and make it '9' and positions don't change
+    string s2 = s;
+    char f = s2[0];
+    int j = 0;
+    while (s2[j] == '9' && j < s2.size() - 1)
+    {
+        j++;
+        f = s2[j];
+    }
+    for (int i = 0; i < s2.size(); i++)
+    {
+        if (s2[i] == f)
+            s2[i] = '9';
+    }
+    maxi = max(maxi, stoi(s2));
+
+    return maxi - mini;
+}
+
+int main()
+{
+    // cout<<minMaxDifference(11891);
+    cout << minMaxDifference(90);
+}
