fed531d6c26d987de64af5c81bb39257d1d183d7
Hemant Jha <hemantjha2005@gmail.com>
2025-06-21 17:30:25 +0530

    some

--- Codeforces/Lists/TLE_Eliminators/900/11/main2.cpp
+++ Codeforces/Lists/TLE_Eliminators/900/11/main2.cpp
@@ -13,20 +13,31 @@ void solve()
     }
 
     // a[i+1]%a[i] != 0
-    for (int i = 1; i < n - 1; i++)
+
+    // making first element even
+    if (a[0] % 2 == 1)
+        a[0]++;
+
+    // first element is even
+    for (int i = 0; i < n; i++)
     {
-        while (a[i + 1] % a[i] == 0 || a[i] % a[i - 1] == 0)
-        {
-            a[i]++;
+        if (i % 2 == 0)
+        { // 0,2,4,6
+            if (a[i] % 2 != 0)
+                a[i]++;
         }
+        else
+        { // 1,3,5,7
+            if (a[i] % 2 == 0)
+                a[i]++;
+        }
+    }
 
-        // for (int i = 1; i < n-1; i++)
-        // {
-        //     while (a[i] % a[i - 1] == 0){
-
-        //         a[i-1]++;
-        //     }
-        // }
+    // checking if same elements are not adjacent
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] == a[i + 1])
+            a[i + 1] += 2;
     }
     for (int i = 0; i < n; i++)
     {
---
+++ Codeforces/Lists/TLE_Eliminators/900/11/notes.md
@@ -0,0 +1,11 @@
+a[i+1]%a[i] != 0 for i in range (0, n-2)
+at most 2n operations
+
+## Intuition
+
+for this array, i just have to add if the next element is divisible by the current element.
+
+## Method 2
+- i can just make the odd and even.
+this method was wrong/flawed.
+
