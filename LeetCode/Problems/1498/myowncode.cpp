84205151d49c66f6ca73dda6c062bee51b2b9565
Hemant Jha <hemantjha2005@gmail.com>
2025-06-29 17:19:50 +0530

    something

---
+++ LeetCode/Problems/1498/myowncode.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isless(int al, int ar, int target)
+{
+    return (al + ar) <= target;
+}
+
+int numSubseq(vector<int> &nums, int target)
+{
+    const long long MOD = 1e9 + 7;
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    long long ans = 0;
+
+    // two pointers
+    int i = 0, j = 0;
+    while (i < n && j < n)
+    {
+        cout << "i: " << i << " " << "j: " << j;
+        if (isless(nums[i], nums[j], target))
+        {
+            cout << " yes";
+            if (i == j)
+                ans++;
+            else
+                ans += abs(j - i + 1);
+            j++;
+        }
+        else
+            i++;
+        cout << endl;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {3, 4, 6, 8};
+    int target = 10;
+    cout << numSubseq(nums, target);
+}
---
+++ LeetCode/Problems/1498/soln.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int numSubseq(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    const int MOD = 1e9 + 7;
+    vector<int> pow2(n);
+    pow2[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        pow2[i] = (pow2[i - 1] * 2) % MOD;
+    }
+
+    int left = 0, right = n - 1;
+    long long ans = 0;
+
+    while (left <= right)
+    {
+        if (nums[left] + nums[right] <= target)
+        {
+            // All subsequences starting with nums[left] in range [left, right]
+            ans = (ans + pow2[right - left]) % MOD;
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+    
+}
