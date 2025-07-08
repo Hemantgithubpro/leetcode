669fcf434eed520dd42bd217d66a99ddd298655a
Hemant Jha <hemantjha2005@gmail.com>
2025-07-08 23:10:36 +0530

    tle

--- Codeforces/Lists/TLE_Eliminators/900/20/main.cpp
+++ Codeforces/Lists/TLE_Eliminators/900/20/main.cpp
@@ -1,11 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
+// after a long time, did some question on my own.
 
-void solve(){
-    // code
+
+int ab(string s)
+{
+    int count = 0;
+    for (int i = 0; i < s.size() - 1; i++)
+    {
+        if (s[i] == 'a' && s[i + 1] == 'b')
+            count++;
+    }
+    return count;
+}
+int ba(string s)
+{
+    int count = 0;
+    for (int i = 0; i < s.size() - 1; i++)
+    {
+        if (s[i] == 'b' && s[i + 1] == 'a')
+            count++;
+    }
+    return count;
 }
 
-int main() {
+void solve()
+{
+    string s;
+    cin >> s;
+    if (ab(s) == ba(s))
+        cout << s << endl;
+    else
+    {
+        if (ab(s) > ba(s))
+        { // to decrease one ab
+            // either change front or last
+            if (s[0] == 'a')
+                s[0] = 'b';
+            else if (s[s.size() - 1] == 'b')
+                s[s.size() - 1] = 'a';
+        }
+        else
+        { // to decrease one ba
+            if (s[0] == 'b')
+                s[0] = 'a';
+            else if (s[s.size() - 1] == 'a')
+                s[s.size() - 1] = 'b';
+        }
+        cout << s << endl;
+    }
+}
+
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -17,7 +64,8 @@ int main() {
     // Your code goes here
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         // input
         solve();
     }
