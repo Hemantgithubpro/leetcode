99a6d395e77608a6be20ace33db20f9b934abd7e
Hemant Jha <hemantjha2005@gmail.com>
2025-07-13 12:11:18 +0530

    try

---
+++ LeetCode/Problems/3355/myowncode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// brute force: O(q*n)
+
+
+// optimal:
+// prefix sum
+bool isZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+{
+    
+}
+
+int main()
+{
+}
