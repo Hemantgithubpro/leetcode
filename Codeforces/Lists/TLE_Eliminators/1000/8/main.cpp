e94d3aee8bf5458a6130eb9d57c514199c0840aa
Hemant Jha <hemantjha2005@gmail.com>
2025-07-21 22:57:25 +0530

    cf

--- Codeforces/Lists/TLE_Eliminators/1000/8/main.cpp
+++ Codeforces/Lists/TLE_Eliminators/1000/8/main.cpp
@@ -3,8 +3,37 @@
 
 using namespace std;
 
-void solve(){
+void solve()
+{
     // input here only
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0;
+    unordered_set<char> usleft, usright;
+    vector<int> preleft(n), preright(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        usleft.insert(s[i]);
+        preleft[i] = usleft.size();
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        usright.insert(s[i]);
+        // preright.push_back(usright.size());
+        preright[i] = usright.size();
+    }
+
+    // now putting dividers at n-1 positions
+    for (int i = 0; i < n - 1; i++)
+    {
+        int temp = preleft[i] + preright[i + 1];
+        ans = max(ans, temp);
+    }
+    cout << ans << endl;
     
 }
 
@@ -12,7 +41,8 @@ void solve(){
 
 // }
 
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -23,7 +53,8 @@ int main() {
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         // ll n;
         // cin>>n;
         // vector<ll> a(n);
@@ -32,7 +63,7 @@ int main() {
         //     cin>>a[i];
         // }
         // solve(n,a);
-        
+
         solve();
     }
 
