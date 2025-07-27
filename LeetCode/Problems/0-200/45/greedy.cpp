6ecea139cd9f14f49887a2d790197063f826f815
Hemant Jha <hemantjha2005@gmail.com>
2025-07-27 17:25:05 +0530

    lc

---
+++ LeetCode/Problems/0-200/45/greedy.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// did with greedy, not with dp. do it with dp later
+int jump(vector<int> &nums)
+{
+    int jumps = 0, l = 0, r = 0;
+
+    while (r < nums.size() - 1)
+    {
+        int farthest = 0;
+        for (int i = l; i <= r; i++)
+            farthest = max(farthest, i + nums[i]);
+        l = r + 1;
+        r = farthest;
+        jumps++;
+    }
+    return jumps;
+}
