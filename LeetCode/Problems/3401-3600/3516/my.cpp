890dc9413cff91fbff63ddd4f89d463283a8bbec
Hemant Jha <hemantjha2005@gmail.com>
2025-09-04 12:24:40 +0530

    lc potd

---
+++ LeetCode/Problems/3401-3600/3516/my.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findClosest(int x, int y, int z)
+{
+    int a = abs(z - x);
+    int b = abs(z - y);
+    if (a == b)
+        return 0;
+    else if (a > b)
+        return 2;
+    else
+        return 1;
+}
