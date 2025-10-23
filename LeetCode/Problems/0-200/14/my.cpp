ee4d81dbdcc808f4498c524054bdc742b525eec2
Hemant Jha <hemantjha2005@gmail.com>
2025-10-23 12:09:46 +0530

    lc

---
+++ LeetCode/Problems/0-200/14/my.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// did it in python, pending for proper learning
+string longestCommonPrefix(vector<string> &strs)
+{
+    string ans="";
+
+    for(int i=0; i<strs[0].size(); i++){
+        for(int j=0; j<strs.size(); j++){
+            // if(i>=strs[j].size())
+        }
+    }
+}
+
+int main()
+{
+}
