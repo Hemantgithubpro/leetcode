d219108c4d416e198ff449f5577d1a6b1163c82c
Hemant Jha <hemantjha2005@gmail.com>
2025-12-07 19:41:11 +0530

    tle

--- Codeforces/Lists/TLE_Eliminators/1000/18/main.cpp
+++ Codeforces/Lists/TLE_Eliminators/1000/18/main.cpp
@@ -2,17 +2,46 @@
 #define ll long long
 
 using namespace std;
-
-void solve(){
+// more of a geometry problem
+// this was an easy one, just had to read the question correctly, and visualize better.
+void solve()
+{
     // input here only
-    
+    ll w, h;
+    cin >> w >> h;
+    // 4 cases: choosing base as top,bottom,left,right(t,b,l,r)
+    ll area = 0;
+    for (ll i = 0; i < 4; i++)
+    {
+        ll k;
+        cin >> k;
+        ll first, last;
+        for (ll j = 0; j < k; j++)
+        {
+            ll x;
+            cin >> x;
+            if (j == 0)
+                first = x;
+            if (j == k - 1)
+                last = x;
+        }
+        ll base = last - first;
+        ll height;
+        if (i <= 1)
+            height = h;
+        else
+            height = w;
+        area = max(area, base * height);
+    }
+    cout << area << endl;
 }
 
 // void solve(ll n, vector<ll>& a){
 
 // }
 
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -23,7 +52,8 @@ int main() {
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         // ll n;
         // cin>>n;
         // vector<ll> a(n);
@@ -32,7 +62,7 @@ int main() {
         //     cin>>a[i];
         // }
         // solve(n,a);
-        
+
         solve();
     }
 
