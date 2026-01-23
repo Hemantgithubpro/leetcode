3b0c5776e4e922b9d8fb81e4498551fd9aeaa98f
Hemant Jha <hemantjha2005@gmail.com>
2026-01-23 22:08:02 +0530

    cf contest

---
+++ Codeforces/Contests/2189/A/better.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define pb push_back
+#define asort(a) sort(a.begin(), a.end())
+#define vsort(a) sort(a.rbegin(), a.rend())
+#define toLow transform(s.begin(), s.end(), s.begin(), ::tolower)
+bool isPowerOfTwo(int n)
+{
+    if (n == 0)
+        return false;
+    return (ceil(log2(n)) == floor(log2(n)));
+}
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+#define floop(n) for (int i = 0; i < n; i++)
+#define floop2(n) for (int j = 0; j < n; j++)
+#define all(a) a.begin(), a.end()
+#define vec(a, n) vector<int> a(n)
+#define cin(a, n)               \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i]
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+const int N = 1e7 + 10;
+using ll = long long;
+using namespace std;
+
+void solve()
+{
+    int n, h, l;
+    cin >> n >> h >> l;
+    int row = 0;
+    int col = 0;
+    int both = 0;
+    int limit = min(h, l);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        if (x <= h)
+            row++;
+        if (x <= l)
+            col++;
+        if (x <= limit)
+            both++;
+    }
+    int x = row + col - both;
+    int ans = min(row, col);
+    ans = min(ans, x / 2);
+    cout << ans << '\n';
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ Codeforces/Contests/2189/D1/better.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define pb push_back
+#define asort(a) sort(a.begin(), a.end())
+#define vsort(a) sort(a.rbegin(), a.rend())
+#define toLow transform(s.begin(), s.end(), s.begin(), ::tolower)
+bool isPowerOfTwo(int n)
+{
+    if (n == 0)
+        return false;
+    return (ceil(log2(n)) == floor(log2(n)));
+}
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+long long mod_pow(long long a, long long b)
+{
+    long long res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+#define floop(n) for (int i = 0; i < n; i++)
+#define floop2(n) for (int j = 0; j < n; j++)
+#define all(a) a.begin(), a.end()
+#define vec(a, n) vector<int> a(n)
+#define cin(a, n)               \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i]
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+const int N = 1e7 + 10;
+using ll = long long;
+using namespace std;
+
+void solve()
+{
+    int n, c;
+    cin >> n >> c;
+    string s;
+    cin >> s;
+
+    if (s[0] != '1' || s[n - 1] != '1')
+    {
+        cout << "-1\n";
+        return;
+    }
+
+    int ones = count(s.begin(), s.end(), '1');
+
+    // f(s) = 2^{ones-1} * (n-ones)
+    long long ans = mod_pow(2, ones - 1);
+    for (int i = 1; i <= n - ones; i++)
+    {
+        ans = ans * i % mod;
+    }
+
+    if (ans % c == 0)
+        cout << "-1\n";
+    else
+        cout << ans << "\n";
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
