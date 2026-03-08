f2bdce3c13a2bf2b142f9f616234558d92a520af
Hemant Jha <hemantjha2005@gmail.com>
2026-03-08 11:17:18 +0530

    tle

--- Codeforces/Lists/TLE_Eliminators/1100/10/better.cpp
+++ Codeforces/Lists/TLE_Eliminators/1100/10/better.cpp
@@ -38,10 +38,80 @@ const int N = 1e7 + 10;
 using ll = long long;
 using namespace std;
 
-
 void solve()
 {
-    
+    int n, x;
+    cin >> n >> x;
+    // vec(a, n);
+    // cin(a, n);
+    // vec(b, n);
+    // cin(b, n);
+    // vec(c, n);
+    // cin(c, n);
+
+    // stack<int> as,bs,cs;
+    // for(int i=n-1; i>=0; i--){
+    //     as.push(a[i]);
+    //     bs.push(b[i]);
+    //     cs.push(c[i]);
+    // }
+
+    // int know=0;
+
+    // while(know<=x){
+    //     if(know==x){
+    //         yes; return;
+    //     }
+    //     int av=as.top();
+    //     int bv=bs.top();
+    //     int bv=bs.top();
+
+    // }
+    // if(know>x) no;
+
+    vector<int> pre[3];
+
+    // Iterate over each stack
+    for (int i = 0; i < 3; i++)
+    {                        // Loop for 3 stacks
+        int s = 0;           // Initialize current OR result
+        pre[i].push_back(s); // Start with 0 OR result
+
+        // Read each book's difficulty in the stack
+        for (int j = 0; j < n; j++)
+        {
+            int a;
+            cin >> a;
+
+            // If adding this book changes the OR result, update it
+            if ((s | a) != s)
+            {
+                s |= a;
+                pre[i].push_back(s);
+            }
+        }
+    }
+
+    bool ans = false; // Flag to check if x can be achieved
+
+    // Check all combinations of OR results from the three stacks
+    for (int A : pre[0])
+    { // Iterate over precomputed OR results of stack 1
+        for (int B : pre[1])
+        { // Iterate over precomputed OR results of stack 2
+            for (int C : pre[2])
+            { // Iterate over precomputed OR results of stack 3
+                // Check if the combined OR result equals x
+                if ((A | B | C) == x)
+                {
+                    ans = true;
+                }
+            }
+        }
+    }
+
+    // Output the result for the current test case
+    cout << (ans ? "YES\n" : "NO\n");
 }
 int32_t main()
 {
