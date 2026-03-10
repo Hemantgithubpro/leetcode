594a408df842ae2a2405a3d46dc2ec4695f6c6f4
Hemant Jha <hemantjha2005@gmail.com>
2026-03-10 23:06:45 +0530

    t;e

--- Codeforces/Lists/TLE_Eliminators/1100/13/main.cpp
+++ Codeforces/Lists/TLE_Eliminators/1100/13/main.cpp
@@ -1,10 +1,39 @@
 #include <bits/stdc++.h>
 #define ll long long
-
+#define mod 1000000007
 using namespace std;
 
 void solve(){
-    
+    int n;
+	cin >> n;
+
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end()); 
+
+	vector<int> b(n);
+	for (int i = 0; i < n; i++) { 
+		cin >> b[i];
+	}
+
+	sort(b.rbegin(), b.rend());
+
+	ll result = 1;
+
+	for (int i = 0; i < n; i++) { 
+		// Find the number of elements in a that are greater than b[i]
+		ll temp = upper_bound(a.begin(), a.end(), b[i]) - a.begin();
+		ll count = a.size() - temp;
+
+		// Update the result with the number of valid positions for a[i]
+		result = result * max(count - i, 0LL) % mod;
+	}
+
+	cout << result << endl;
+
 }
 
 
