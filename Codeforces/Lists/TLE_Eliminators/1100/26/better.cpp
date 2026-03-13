e5c4a76f229b1e4970cd1635005fd0fc71b408f3
Hemant Jha <hemantjha2005@gmail.com>
2026-03-13 20:24:34 +0530

    my soln

--- Codeforces/Lists/TLE_Eliminators/1100/26/better.cpp
+++ Codeforces/Lists/TLE_Eliminators/1100/26/better.cpp
@@ -28,9 +28,9 @@ bool isPrime(int n)
 #define floop(n) for (int i = 0; i < n; i++)
 #define floop2(n) for (int j = 0; j < n; j++)
 #define all(a) a.begin(), a.end()
-#define vec(a, n) vector<int> a(n)
-#define cin(a, n)               \
-    for (int i = 0; i < n; i++) \
+#define vec(a, n) vector<ll> a(n)
+#define cin(a, n)              \
+    for (ll i = 0; i < n; i++) \
     cin >> a[i]
 #define yes cout << "YES" << endl
 #define no cout << "NO" << endl
@@ -38,10 +38,61 @@ const int N = 1e7 + 10;
 using ll = long long;
 using namespace std;
 
-
 void solve()
 {
-    
+    ll n;
+    cin >> n;
+    vec(a, n);
+    cin(a, n);
+
+    // prefix sum
+    vector<ll> psum(n, 0);
+    psum[0] = a[0];
+    for (ll i = 1; i < n; i++)
+    {
+        psum[i] = psum[i - 1] + a[i];
+    }
+
+    // two pointer
+    ll l = 0, r = n - 1;
+    ll sumall = psum[r];
+    ll summid = sumall / 2;
+    ll mid = l + (r - l) / 2;
+    while (l < r)
+    {
+        if (psum[mid] == summid)
+            break;
+        else if (psum[mid] > summid)
+            r = mid - 1;
+        else
+            l = mid + 1;
+
+        mid = l + (r - l) / 2;
+    }
+
+    ll sum1 = 0, sum2 = 0;
+    l = mid;
+    r = mid + 1;
+    while (l >= 0 && r < n)
+    {
+        sum1 = psum[l];
+        sum2 = sumall - psum[r - 1];
+        if (sum1 == sum2)
+            break;
+
+        if (sum1 > sum2)
+        {
+            sum1 -= a[l];
+            l--;
+        }
+        else
+        {
+            sum2 -= a[r];
+            r++;
+        }
+    }
+
+    cout << (l+1) + (n-r) << endl;
 }
 int32_t main()
 {
