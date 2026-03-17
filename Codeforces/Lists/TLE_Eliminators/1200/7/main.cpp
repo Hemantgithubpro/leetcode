83cc0d2f2bec787a4d23d940715e8e24e195e622
Hemant Jha <hemantjha2005@gmail.com>
2026-03-17 22:49:06 +0530

    tle

--- Codeforces/Lists/TLE_Eliminators/1200/7/main.cpp
+++ Codeforces/Lists/TLE_Eliminators/1200/7/main.cpp
@@ -8,7 +8,7 @@ using ll = long long;
 #define floop(n) for (int i = 0; i < n; i++)
 #define floop2(n) for (int j = 0; j < n; j++)
 #define all(a) a.begin(), a.end()
-#define vec(a, n) vector<ll> a(n)
+#define vec(a, n) vector<int> a(n)
 #define cin(a, n)               \
     for (int i = 0; i < n; i++) \
     cin >> a[i]
@@ -39,9 +39,70 @@ bool isPrime(int n)
     return true;
 }
 
+ll calculate(vector<vector<int>> &a, int row1, int row2)
+{
+    int m = a[row1].size();
+    ll ans = 0;
+    for (int i = 0; i < m; i++)
+    {
+        ans += abs(a[row1][i] - a[row2][i]);
+    }
+    return ans;
+}
+
 void solve()
 {
-    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int temp;
+            cin >> temp;
+            a[i].push_back(temp);
+        }
+    }
+
+    // floop(n){
+    //     asort(a[i]);
+    // }
+
+    // brute force
+    // 1: 2,3,4,,,n
+    // 2: 3,4,5,,,n
+
+    ll ans = 0;
+    // for(int i=0; i<n-1; i++){
+    //     for(int j=i+1; j<n; j++){
+    //         ans+=calculate(a,i,j);
+    //     }
+    // }
+    // cout<<ans<<endl;
+
+    for (int i = 0; i < m; i++)
+    {
+        // column m
+        vector<int> col(n);
+        for (int j = 0; j < n; j++)
+        {
+            col[j] = a[j][i];
+        }
+        asort(col);
+
+        ll temp = 0;
+        for (int k = 0; k < n; k++)
+        {
+            int val = col[k];
+            // formula: add val times the index+1, and subtract val times the n-i-1
+            // temp += (val * (k + 1));
+            temp += 1LL * val * k;
+            temp -= 1LL * val * (n - k - 1);
+        }
+        ans += temp;
+    }
+    cout << ans << endl;
 }
 
 int32_t main()
