58afc896e816228a0de33656b197187881cfbbad
Hemant Jha <hemantjha2005@gmail.com>
2026-06-18 23:51:53 +0530

    another 2 pointer

--- LeetCode/Problems/0-200/26/myowncode1.cpp
+++ LeetCode/Problems/0-200/26/myowncode1.cpp
@@ -1,21 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int removeDuplicates(vector<int>& nums) {
-    set<int> a;
-    for (int n : nums) {
-        a.insert(n);
-    }
-    vector<int> ans;
-    for (auto it = a.begin(); it != a.end(); it++) {
-        ans.push_back(*it);
-    }
-    nums=ans;
-    return a.size();
-}
+class Solution
+{
+public:
+    // takes space
+    // int removeDuplicates(vector<int>& nums) {
+    //         vector<int> a;
+    //         a.push_back(nums[0]);
+    //         int temp=nums[0];
+    //         for(int i=1; i<nums.size(); i++){
+    //             if(nums[i]==temp) continue;
+    //             else{
+    //                 temp=nums[i];
+    //                 a.push_back(temp);
+    //             }
+    //         }
+    //         nums=a;
+    //         return a.size();
+    // }
 
-int main(){
+    int removeDuplicates(vector<int> &nums)
+    {
+        // two pointer
+        int n = nums.size();
+        int start = 0;
+        int i = 1;
+        if (n == 1)
+            return 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[start] != nums[i])
+            {
+                start++;
+                nums[start] = nums[i];
+            }
+        }
+        return start + 1;
+    }
+};
+
+int main()
+{
     // this was the brute force solution, not the most optimal solution, next time I'll try two pointer approach, to do within just one for loop.
-    vector<int> nums={2,5,2,7,1};
-    cout<<removeDuplicates(nums); 
+    vector<int> nums = {2, 5, 2, 7, 1};
+    // cout<<removeDuplicates(nums);
 }
