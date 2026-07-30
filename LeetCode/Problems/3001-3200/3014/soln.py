3fbea73298fb52ca77ed3861a3e751b473c982a8
Hemant Jha <hemantjha2005@gmail.com>
2026-07-30 23:39:20 +0530

    soln

---
+++ LeetCode/Problems/3001-3200/3014/soln.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        # since distinct letters, so no need to check for duplicates
+        # first 8:1, next 8:2, next 8:3, next 8:4
+        ans: int = 0
+        for i in range(n):
+            ans += i // 8 + 1
+        return ans
+
+
+    
+    
+def main():
+    sol = Solution()
+    # word = "abcdefghijklmnopqrstuvwxyz"
+    word = "xycdefghij"
+    print(sol.minimumPushes(word))
+    
+if __name__ == "__main__":
+    main()
