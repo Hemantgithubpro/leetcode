0a031fcdbe9f5a9186499bdff52941d3a18cac25
Hemant Jha <hemantjha2005@gmail.com>
2026-08-02 22:12:16 +0530

    lc

---
+++ LeetCode/Problems/401-600/486/accessing-list.py
@@ -0,0 +1,14 @@
+from ast import List
+
+
+
+
+# making list in python
+my_list = [1, 2, 3, 4, 5]
+
+# accessing list elements
+# for i in range(0, len(my_list), 1):
+    # print(my_list[i])
+
+def counteven(self, nums: List[int]) -> bool:
+    
